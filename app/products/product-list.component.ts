c744456a2a37794cd83d91a169b25b1c4474f281
tc-frontend <roberto.loreto@avanade.com>
2017-02-20 14:05:21 +0100

    +Add a click event binding in Show Image button +Add ngId directive in img tag to show or not the image ~Change the button name using interpolation

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent   {
     pageTitle: string = 'Product List';
     imageWith: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
     products: any[] = [
         {
             "productId": 2,
@@ -30,5 +31,9 @@ export class ProductListComponent   {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 
 }
